74acdce96d3ad7848f95587cb4725a65dac7142f
liangjlgit <107822802+liangjlgit@users.noreply.github.com>
2022-11-29 22:12:34 +0800

    Delete CoDF.R

--- CoDF.R
+++
@@ -1,9 +0,0 @@
-CoDF <- function(da, daa) {
-  up <- upper.tri(da)
-  data.frame(
-    from = rownames(da)[col(da)[up]],
-    to = rownames(da)[row(da)[up]],
-    corr  =(da)[up],
-    p = daa[up]
-  )
-}
